51af192df46b59fcafe8f2cb125792ff3150f7ac
ms161 <msehrawat161@gmail.com>
2023-12-15 23:27:54 +0530

    task 2

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express=require('express')
-
+const bodyParser=require('body-parser')
 
 const app=express()
+app.use(bodyParser.urlencoded({extended:false}))
 
- app.use((req,res,next)=>{
-    res.send('this is express')
-    next()
+ app.use('/addproduct',(req,res,next)=>{
+   res.send('<form action="/product" method="POST"> <input type="text" name="title">  <input type="text" name="title"> <button>Add product</button>  </form>')
  })
 
+ app.use('/product',(req,res,next)=>{
+   console.log(req.body)
+   res.redirect('/') 
+ })
+
+app.use('/',(req,res,next)=>{
+res.send('this is express')
+})
  
+
  app.listen(3000)
 
